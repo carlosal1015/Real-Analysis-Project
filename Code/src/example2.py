854d87ecebdacff7d2a2fd1c67011ff9c1096c8b
Oromion <caznaranl@uni.pe>
2019-06-10 17:19:45 -0500

    Update

--- Code/src/example2.py
+++ Code/src/example2.py
@@ -1,6 +1,9 @@
+"""
+Ejemplos de la página web
+http://timescalewiki.org/index.php/Timescalecalculus_python_library_documentation
+"""
 from timecalculus import timescalecalculus as tsc
-from fractions import Fraction
 
-ts = timescale([1, 2, 3, 4, 5])
+ts = tsc.timescale([1, 2, 3, 4, 5])
 
 #print(ts.dexpf(lambda x: 1, 3, 1))
